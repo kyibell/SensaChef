688055b921ab8f2fd1b4a86cb979871cd253997a
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-02-28 12:42:42 -0600

    Loaded config settings in tts and added a class extending the pydantic base model for text input and output audio file type

--- backend/app/routers/tts.py
+++ backend/app/routers/tts.py
@@ -12,3 +12,15 @@ load_dotenv()
 AWS_AK = os.environ.get("AWS_AK")
 AWS_SAK = os.environ.get("AWS_SAK")
 router = APIRouter()
+
+#load config settings
+@lru_cache()
+def get_settings():
+    return config.Settings()
+
+#class for text input as well as output audio file type
+class Text(BaseModel):
+    content: str
+    output_format: str
+
+
