4c94af25c8116d54a41c3469ace35601ee3f3d76
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-03-01 19:32:02 -0600

    Finished the useEffect hook

--- frontend/src/hooks/useSpeechToText/index.jsx
+++ frontend/src/hooks/useSpeechToText/index.jsx
@@ -5,6 +5,47 @@ const useSpeechToText = (options) => {
     const [transcript, setTranscript] = useState("")
     const recognitionRef = useRef(null) 
 
+    useEffect(()=>{
+        // Check if web speech api is supported
+        if(!('webkitSpeechRecognition' in window)){
+            console.error("Web speech api is not supported.")
+            return;
+        }
+        recognitionRef.current = new window.webkitSpeechRecognition()
+        const recognition = recognitionRef.current
+        recognition.interimResults = options.interimResults || true
+        recognition.lang = options.lang || "en-US"
+        recognition.continuous = options.continuous || false
+
+        if("webkitSpeechGrammarList" in window){
+            const grammar = "#JSGF V1.0; grammar punctuation; public <punc> = . | , | ? | ! | ; | : ;"
+            const speechRecognitionList = new window.webkitSpeechGrammarList()
+            speechRecognitionList.addFromString(grammar, 1)
+            recognition.grammars = speechRecognitionList
+        }
+
+        recognition.onresult = (event) => {
+            let text = ""
+            for(let i = 0; i < event.results.length; i++){
+                text += event.results[i][0].transcript
+            }
+            setTranscript(text)
+        }
+
+        recognition.onerror = (event) => {
+            console.error("Speech recognition error:", event.error)
+        }
+
+        recognition.onend = () => {
+            setIsListening(false)
+            setTranscript("")
+        }
+
+        return () => {
+            recognition.stop()
+        }
+    }, [])
+
 }
 
 export default useSpeechToText  
