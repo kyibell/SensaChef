59791e27fda85ccd6c138f1d44066d328828ac0f
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-03-25 16:16:31 -0500

    Declared stateful variables for recipes, loading, and error (for displaying the recipes on the frontend)

---
+++ frontend/src/components/HomeUI/RecipeList.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function RecipeList() {
+    const [recipes, setRecipes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    
+
+}
+export default RecipeList
