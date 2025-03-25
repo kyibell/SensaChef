41e41ed2c2b3d78e762efaa7f6a05703222d6cc7
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-03-25 16:22:10 -0500

    Added a function that fetches recipes from localhost:8000. Caught errors if necessary

--- frontend/src/components/HomeUI/RecipeList.jsx
+++ frontend/src/components/HomeUI/RecipeList.jsx
@@ -5,7 +5,28 @@ function RecipeList() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/recipes');
+                if(!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                console.log("API Response: ", data);
+                setRecipes(data);
+                setError(null);
+            } catch (err) {
+                setError(err.message);
+                console.log("Fetch error details: ", err)
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchRecipes();
+    }, []);
+
 
 }
 export default RecipeList
