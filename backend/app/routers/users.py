295076ebdb5fa5112fa7ad66f8c8313cc0bcaa7b
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-03-25 19:01:32 -0500

    Added API endpoints for recipes and recipe steps

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -20,6 +20,22 @@ async def get_all_recipes():
     response = supabase.table("recipes").select("*").execute()
     return response.data
 
+# --------------------------------
+# API endpoints for recipes:
+@router.get("/recipes/{recipe_id}", tags=["recipes"])
+async def get_recipe(recipe_id: int):
+    response = supabase.table("recipes").select("*").eq("id", recipe_id).execute()
+    return response.data[0]
+
+@router.get("/recipes/{recipe_id}/steps", tags=["recipes"])
+async def get_recipe_steps(recipe_id: int, step_num: int = None):
+    query = supabase.table("steps").select("*").eq("recipe_id", recipe_id)
+    if step_num:
+        query = query.eq("step_num", step_num)
+    response = query.execute()
+    return response.data
+# ---------------------------------
+
 @router.post("/users", tags=["users"])
 async def create_user(user: userModel):
     response = supabase.table("users").insert({"name": user.name, "email": user.email, "username": user.username, "password": user.password, "creationDate": (datetime.now(timezone.utc)).isoformat()}).execute()
