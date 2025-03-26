1784b13ef2859685d6f27546d8a9577ca7a1c94c
kyibell <148383705+kyibell@users.noreply.github.com>
2025-03-25 20:15:34 -0500

    imported opencv for camera and added a temp page (Camera.jsx) for testing purposes for the API endpoint

---
+++ backend/app/routers/camera.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Response, render_template
+import cv2 # OpenCV import
+
+# Define the Router
+router = APIRouter()
+
+# Camera Class
+class Camera:
+    def __init__(self):
+        self.video = cv2.VideoCapture(0) # 0 for webcam access
+    def __del__(self):
+        self.video.release() # For Deleting the video/stopping the streaming
+    def get_frames(self): # Reading the video to return the frames
+        ret, frame = self.video.read() # ret = return 
+        if not ret: # If there's no return, error handle
+            print('No Frames.')
+            return b''
+        ret, jpeg = cv2.imencode('.jpg', frame) # encode img format and streams data
+        if not ret:
+            return b''
+        return jpeg.tobytes() # Return each frame as a jpg in bytes
+
+router.get('/')
+def index():
+    return render_template('Camera.jsx')
+
+def generate_camera(camera):
+    while True: # Inf. loop to get frames
+        frame = camera.get_frames() # Get the Frame from the Camera Class
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
+
+
+router.get('/video-feed')
+def get_video_feed():
+    pass
