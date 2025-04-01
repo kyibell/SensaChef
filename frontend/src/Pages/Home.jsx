0cd4389f8da1a5c3d5942663c76fed059bc228ec
TeekshnaaR14 <teekshnaa14ravi@gmail.com>
2025-04-01 13:00:40 -0500

    create homepage

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -4,17 +4,38 @@ import RecipeList from "../components/HomeUI/RecipeList";
 
 
 function Home() {
-
     const navigate = useNavigate();
 
     return(
         <>
-            <h1>This is the home page</h1>
+            <Nav />
+            <div style={{ textAlign: "Center", padding: "20px"}}>
+                <h1 style={{ fontSize: "28px", color: "#000000" }}> Welcome to SensaChef</h1>
+                <h2 style={{ color: "#000000" }}>Discover & Cook Your Favorite Recipes</h2>
+
+
+{/*               <button 
+                    onClick={() => navigate('/cookingmode')}
+                    style={{
+                        padding: "10px 20px",
+                        background: "#ff6347",
+                        color: "white",
+                        border: "none",
+                        borderradius: "5px",
+                        cursor: "pointer",
+                        fontSize: "16px",
+                        marginTop: "10px"
+                    }}
+                >
+                    Click to go into Cooking Mode
+                </button>*/}
+            </div>
+{/*            <h1>Home</h1>
             <h2>Not complete</h2>
-            <button onClick={() => navigate('/cookingmode')} >Click to go into cooking mode</button>
+            <button onClick={() => navigate('/cookingmode')} >Click to go into cooking mode</button> */}
             <RecipeList />
         </>
     );
 }
 
-export default Home
+export default Home;
