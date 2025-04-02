878e48edb2c74c721b1640f9e422af11ee35948f
kyibell <bellkyilee@gmail.com>
2025-04-02 13:28:38 -0500

    fixed server

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.database import supabase
 from .routers import users, camera, recipes
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import uvicorn
 
 app = FastAPI()
 app.include_router(users.router)
@@ -24,3 +26,9 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"message": "Hello, world!"}
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if not set
+    uvicorn.run(app, host="0.0.0.0", port=port)
