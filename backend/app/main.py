71d122bb93327ddc560b607048c95f21d305d32e
kyibell <bellkyilee@gmail.com>
2025-04-07 16:08:05 -0500

    removed camera.py and removed it from main app routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from app.database import supabase
-from .routers import users, camera, recipes, ask
+from .routers import users, recipes, ask
 from fastapi.middleware.cors import CORSMiddleware
 import os
 import uvicorn
 
 app = FastAPI()
 app.include_router(users.router)
-app.include_router(camera.router)
 app.include_router(recipes.router)
 app.include_router(ask.router)
 
