e07a7a8244d146deca993d60d7faa7e7052b3644
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-04-06 19:27:34 -0500

    Fixed a bug in AI model, so that it does not spit out a random response when a user does not enter a prompt

--- frontend/src/components/AiModel/AiModel.jsx
+++ frontend/src/components/AiModel/AiModel.jsx
@@ -47,6 +47,13 @@ function AiModel() {
     setLoading(true);
     setResponse("");
 
+    if(!question) {
+      const noPromptResponse = "Please enter a prompt";
+      setResponse(noPromptResponse);
+      setLoading(false);
+      speak(noPromptResponse);
+      return;
+    }
     try {
       // for development
       // const res = await fetch("http://localhost:8000/api/ask", {
