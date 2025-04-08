b93f60244ab210a5d69c63b48fd5828fe56bfc12
DaiduongDinh05 <daiduongddinh@gmail.com>
2025-04-08 17:48:56 -0500

    Testing workflow

--- backend/app/backend_tests/test_backend.py
+++ backend/app/backend_tests/test_backend.py
@@ -9,3 +9,8 @@ def test_read_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, world!"}
     
+def test_read_all_users():
+    response = client.get("/users")
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
