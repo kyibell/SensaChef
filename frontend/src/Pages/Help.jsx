2fe635a520ca9ed076a9e3743f1224f18b0a1175
TeekshnaaR14 <teekshnaa14ravi@gmail.com>
2025-04-10 03:03:32 -0500

    Integrated Camera.jsx component into frontend and made slight styling tweaks for UI presentation

--- frontend/src/Pages/Help.jsx
+++ frontend/src/Pages/Help.jsx
@@ -1,10 +1,37 @@
-import AiModel from "../components/AiModel/AiModel";
+{/*import AiModel from "../components/AiModel/AiModel";
 import PostList from "../components/Forum/PostList";
 
 export default function Help() {
     return(
         <>
             <AiModel />
+            <PostList />
+        </>
+    );
+}*/}
+
+import { Link } from "react-router-dom";
+import AiModel from "../components/AiModel/AiModel";
+import PostList from "../components/Forum/PostList";
+
+export default function Help() {
+    return (
+        <>
+            <AiModel />
+            <div style={{ display: "flex", justifyContent: "flex-end", margin: "1rem" }}>
+                <Link to="/create-post">
+                    <button style={{
+                        padding: "10px 20px",
+                        backgroundColor: "#4CAF50",
+                        color: "white",
+                        border: "none",
+                        borderRadius: "5px",
+                        cursor: "pointer"
+                    }}>
+                        Create Post
+                    </button>
+                </Link>
+            </div>
             <PostList />
         </>
     );
