6babed81bc6074bccc19d83ca95552feaf81c025
kyibell <bellkyilee@gmail.com>
2025-04-11 20:15:41 -0500

    pipeline test

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.include_router(ask.router)
 app.include_router(AiNav.router)
 
 app.include_router(posts.router)
-app.include_router(comments.router)
+app.include_router(comments.router) # test
 
 origins = [
     "http://localhost:3000"
