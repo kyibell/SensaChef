4bc6baddeb25491eeca83bfc6e8d5cc3d8b689fa
kyibell <bellkyilee@gmail.com>
2025-04-14 15:14:33 -0500

    added basic logic for creation of recipes

--- backend/app/routers/recipes.py
+++ backend/app/routers/recipes.py
@@ -57,9 +57,28 @@ async def get_recipe_by_name(recipe_name: str):
 
 
 # Create A Recipe
-@router.post("/create_recipe", tags=["recipes"])
-async def create_recipe():
-    pass
+@router.post("/create_recipe", tags=["recipes"],status_code=201)
+async def create_recipe(recipe: Recipe, user_id: UUID):
+    try:
+        db_user = supabase.table("users").select("*").eq("id", user_id)
+        if not db_user:
+            raise HTTPException(status_code=404, detail="User Not Found.")
+        recipe_data = {
+            "recipe-name": recipe.recipe_name,
+            "image_url": recipe.recipe_image,
+            "image_tags": recipe.recipe_tags,
+            "description": recipe.recipe_description,
+            "user_id": str(recipe.user_id)
+        }
+
+        response = supabase.table("recipes").insert(recipe_data).execute()
+
+        if response:
+            return response
+        else:
+            raise HTTPException(status_code=400, detail="Error creating recipe")
+    except Exception as error:
+        raise HTTPException(status_code=500, detail="Internal Server Error.")
 
 # Update A Recipe
 @router.put("/update_recipe/{recipe_id}", tags=["recipes"])
