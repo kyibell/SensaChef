e2c0bc2c9c49d3076a903aa8757d21c4ae84253a
kyibell <bellkyilee@gmail.com>
2025-04-16 16:39:53 -0500

    added supabase storage for recipe images for the creation of recipes

--- backend/app/routers/recipes.py
+++ backend/app/routers/recipes.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException
-from app.database import supabase
+from fastapi import APIRouter, HTTPException, UploadFile, Form
+from app.database import supabase, admin_supabase
 from pydantic import BaseModel # For Input Validation
 from uuid import UUID
+import os
 
 router = APIRouter() 
 
@@ -58,27 +59,53 @@ async def get_recipe_by_name(recipe_name: str):
 
 # Create A Recipe
 @router.post("/create_recipe", tags=["recipes"],status_code=201)
-async def create_recipe(recipe: Recipe, user_id: UUID):
+async def create_recipe(user_id: UUID, 
+                        image: UploadFile,
+                        title: str = Form(),
+                        description: str = Form(),
+                        recipe_tags: list[str] = Form(),
+                        ):
     try:
         db_user = supabase.table("users").select("*").eq("id", user_id)
         if not db_user:
             raise HTTPException(status_code=404, detail="User Not Found.")
+        # Get a File name for Storage
+        file_extension = image.filename.split(".")[-1] # Fetch the file extension from the image upload
+        file_name = f"recipes/{user_id}/{title}.{file_extension}"
+
+        img_file = await image.read()
+
+        image_response = (
+            admin_supabase.storage
+            .from_('recipe-images')
+            .upload(
+                file=img_file,
+                path=file_name,
+                file_options={"content-type": 'image/jpeg'}
+            )
+        )
+
+        if not image_response:
+            raise HTTPException(status_code=500, detail="Error Uploading Image")
+        
+        recipe_image = admin_supabase.storage.from_('recipe-images').get_public_url(file_name)
+
         recipe_data = {
-            "recipe-name": recipe.recipe_name,
-            "image_url": recipe.recipe_image,
-            "image_tags": recipe.recipe_tags,
-            "description": recipe.recipe_description,
-            "user_id": str(recipe.user_id)
+            "recipe-name": title,
+            "image_url": recipe_image,
+            "image_tags": recipe_tags,
+            "description": description,
+            "user_id": str(user_id)
         }
 
-        response = supabase.table("recipes").insert(recipe_data).execute()
+        response = admin_supabase.table("recipes").insert(recipe_data).execute()
 
         if response:
             return response
         else:
             raise HTTPException(status_code=400, detail="Error creating recipe")
     except Exception as error:
-        raise HTTPException(status_code=500, detail="Internal Server Error.")
+        raise HTTPException(status_code=500, detail=str(error))
 
 # Update A Recipe
 @router.put("/update_recipe/{recipe_id}", tags=["recipes"])
