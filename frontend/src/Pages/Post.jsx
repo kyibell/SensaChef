5d6a21df165b20cb0370d6b029fa95f8d348c963
TeekshnaaR14 <teekshnaa14ravi@gmail.com>
2025-04-24 16:25:18 -0500

    Styled Post page and comment layout with the images

--- frontend/src/Pages/Post.jsx
+++ frontend/src/Pages/Post.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import CreateComment from '../components/Forum/CreateComment';
+import '../components/Forum/PostComment.css';
 
 function Post({}) {
 
